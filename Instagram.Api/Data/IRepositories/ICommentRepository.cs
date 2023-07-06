6702aff592655f6b92fefd1fef87d7e3a725a38b
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-07-06 20:30:15 +0500

    Repository and IRepository changes

--- Instagram.Api/Data/IRepositories/ICommentRepository.cs
+++ Instagram.Api/Data/IRepositories/ICommentRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace Instagram.Api.Data.IRepositories
+﻿using Instagram.Api.Domain.Entities;
+
+namespace Instagram.Api.Data.IRepositories
 {
-    public class ICommentRepository
+    public interface ICommentRepository
     {
+        public Post Create(Post post);
+
+        public Post Update(Post post);
+
+        public bool Delete(Post post);
+
+        public Post GetById(long id);
+
+        public IQueryable<Post> GetAll();
     }
 }
